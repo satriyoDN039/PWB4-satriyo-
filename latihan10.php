b7d53dec55864963e466d899f36d6e9149fb3985
PWB_SATRYO NURAJI RASA <92238297+satriyoDN039@users.noreply.github.com>
2021-11-18 21:30:21 +0700

    Add files via upload

---
+++ latihan10.php
@@ -0,0 +1,23 @@
+<?php
+$destination="Tokyo";
+Switch ($destination){
+case "CI away":
+echo "Bring an extra $500";
+break;
+case "Ciherang":
+echo "Bring an open mind";
+break;
+case "Bogor":
+
+
+echo "Bring 15 bottles of SPF 50 Sunscreen";
+break;
+case "Tokyo":
+echo "Bring lots of money";
+break;
+case "Caribbean Islands":
+echo "Bring a swimsuit";
+break;
+
+}
+?>
